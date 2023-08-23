7c713e585cb72f1d0fc9a5f9c34b966ba48432b0
yunko006 <peyrachethomas@gmail.com>
2023-08-23 16:01:10 +0200

    Refractor: main to db.py to init db

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,4 +1,11 @@
 import os
+import logging
+
+from tortoise import Tortoise, run_async
+from tortoise.contrib.fastapi import register_tortoise
+
+log = logging.getLogger("uvicorn")
+
 
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
@@ -13,3 +20,31 @@ TORTOISE_ORM = {
         },
     },
 }
+
+
+def init_db(app) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise", "app.models.playlist"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+
+
+# faire les migrations sans passer par les cmds aerich
+async def generate_schema() -> None:
+    log.info("Initializing Tortoise...")
+
+    await Tortoise.init(
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise", "app.models.playlist"]},
+    )
+    log.info("Generating database schema via Tortoise...")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+# pour exec la fnc run_async seulement quand cmd "docker-compose exec backend python app/db.py" dans terminal.
+if __name__ == "__main__":
+    run_async(generate_schema())
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,28 +1,31 @@
 import os
-from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from .config import Settings, get_settings
+from .api import ping
+from .config import Settings, log
+from .db import init_db
 
 
-app = FastAPI()
-settings = Settings()
+def create_app() -> FastAPI:
+    app = FastAPI()
+    settings = Settings()
 
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise", "app.models.playlist"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+    app.include_router(ping.router)
+
+    return app
 
 
-@app.get("/ping")
-async def pong(settings: Annotated[Settings, Depends(get_settings)]):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+app = create_app()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
