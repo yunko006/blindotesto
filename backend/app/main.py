e21122d13c0a91383c9df8835a926a84626b4268
yunko006 <peyrachethomas@gmail.com>
2023-08-24 14:10:01 +0200

    Refractor: remove on_event to use lifespan
    startup and shutdown events are deprecated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 import os
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
@@ -8,8 +8,19 @@ from .config import Settings, log
 from .db import init_db
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup event
+    log.info("Chargement de l'application...")
+    init_db(app)
+    log.info("db init...")
+    yield
+    # event after shutdown
+    log.info("Arret de l'app en cours...")
+
+
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
     settings = Settings()
 
     app.include_router(ping.router)
@@ -18,14 +29,3 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
-
-
-@app.on_event("startup")
-async def startup_event():
-    log.info("Starting up...")
-    init_db(app)
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    log.info("Shutting down...")
