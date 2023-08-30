2474329122ddfb7bc49af385f59cae9af92ff33e
yunko006 <peyrachethomas@gmail.com>
2023-08-30 13:40:38 +0200

    Refractor: include routers & use of on_event
    cant set up my init_db func with lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,31 +1,47 @@
+from .db import init_db
+import logging
 import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from .api import ping
-from .config import Settings, log
-from .db import init_db
+from .api import ping, summaries, playlists
+from .config import Settings
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # startup event
-    log.info("Chargement de l'application...")
-    init_db(app)
-    log.info("db init...")
-    yield
-    # event after shutdown
-    log.info("Arret de l'app en cours...")
+log = logging.getLogger("uvicorn")
+
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     # startup event
+#     log.info("Chargement de l'application...")
+#     init_db(app)
+#     log.info("db init...")
+#     yield
+#     # event after shutdown
+#     log.info("Arret de l'app en cours...")
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(lifespan=lifespan)
+    app = FastAPI()  # lifespan=lifespan)
     settings = Settings()
 
     app.include_router(ping.router)
-
+    app.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
+    app.include_router(playlists.router, prefix="/playlists", tags=["playlists"])
     return app
 
 
 app = create_app()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
