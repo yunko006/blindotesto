af1ff4f720c67b6649e2ea3145475ba255e0c09d
yunko006 <peyrachethomas@gmail.com>
2023-08-30 18:41:18 +0200

    Add: tests for get_one_playlist & HTTPException

--- backend/app/api/playlists.py
+++ backend/app/api/playlists.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.models.pydantic import PlaylistPayloadSchema, PlaylistResponseSchema
 from app.crud.base import post, get_all, get_one
 from app.models.playlist import PlaylistSchema
@@ -23,4 +23,8 @@ async def get_all_playlists() -> List[PlaylistSchema]:
 @router.get("/{id}/", response_model=PlaylistSchema)
 async def get_one_playlist(id: int) -> PlaylistSchema:
     playlist = await get_one(id)
+    # ajout pour pouvoir dans une playlist demande n'existe pas dans la db.
+    if not playlist:
+        raise HTTPException(status_code=404, detail="Playlist not found")
+
     return playlist
--- backend/tests/test_playlist.py
+++ backend/tests/test_playlist.py
@@ -36,3 +36,37 @@ def test_create_playlist_invalid_payload(test_app_with_db):
     )
 
     assert response.status_code == 422
+
+
+def test_get_one_playlist(test_app_with_db):
+    payload = {
+        "name": "Test Playlist",
+        "description": "This is a test playlist",
+        "owner": "Test Owner",
+        "spotify_id": "test_spotify_id",
+        "spotify_uri": "test_spotify_uri",
+        "tracks": ["track1", "track2"],
+    }
+
+    response = test_app_with_db.post(
+        "/playlists/",
+        json=payload,
+    )
+
+    playlist_id = response.json()["id"]
+
+    response = test_app_with_db.get(f"/playlists/{playlist_id}")
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["name"] == "Test Playlist"
+    assert response_dict["description"] == "This is a test playlist"
+    assert response_dict["owner"] == "Test Owner"
+    assert response_dict["spotify_id"] == "test_spotify_id"
+
+
+def test_get_one_invalid_playlist(test_app_with_db):
+    playlist_id = 234
+    response = test_app_with_db.get(f"/playlists/{playlist_id}")
+
+    assert response.status_code == 404
