0f4360fcdbbef5fc14a605e68da7e590db4d34f8
yunko006 <peyrachethomas@gmail.com>
2024-12-20 16:59:13 +0100

    Create: next js app from npx + call api from page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,20 @@
+// frontend/app/page.tsx
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Test de connexion avec le backend
+    fetch("http://localhost:8000/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.Message));
+  }, []);
+
+  return (
+    <main className="p-4">
+      <h1>Message from backend: {message}</h1>
+    </main>
+  );
+}
