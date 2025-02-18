a86103338bc7456323d6a4c51f2ef719976bdf4e
yunko006 <peyrachethomas@gmail.com>
2025-02-18 19:17:40 +0100

    Update: urls

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 import os
 
 load_dotenv()
@@ -8,10 +8,10 @@ load_dotenv()
 class Settings(BaseSettings):
     CLIENT_ID: str = os.getenv("CLIENT_ID")
     CLIENT_SECRET: str = os.getenv("CLIENT_SECRET")
-    REDIRECT_URI: str = "http://localhost:8000/callback"
+    REDIRECT_URI: str = "http://localhost:3000/callback"
     # ca je suis pas sur encore que ca soit dans settings.
     auth_url: str = "https://accounts.spotify.com/authorize"
-    auth_url: str = "https://accounts.spotify.com/api/token"
+    token_url: str = "https://accounts.spotify.com/api/token"
     api_base_url: str = "https://api.spotify.com/v1/"
 
 
